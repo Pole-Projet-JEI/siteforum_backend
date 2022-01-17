173837c26c6bac1d16603ef1595e2d19c3b3115a
meriem <meriembenismail@insat.u-carthage.tn>
2022-01-16 16:31:25 -0800

    REST API

--- routes/speakers.js
+++ routes/speakers.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
 import { createSpeaker , getSpeakers , getSpeakerByID , deleteSpeaker , updateSpeaker  } from "../controllers/speaker.js";
 
 const router = express.Router();
 
-let speakers = [];
+
 
 //all routes in here are starting with /users
 router.get('/',getSpeakers);
